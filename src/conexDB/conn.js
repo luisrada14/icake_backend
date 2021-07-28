39c1e9e0cc01f9a1683148c67570843993e07681
gfrancoa <gfrancoa@outlook.com>
2021-07-28 07:28:46 -0500

    se actualiza la ruta de BD a la de mongo atlas

--- src/conexDB/conn.js
+++ src/conexDB/conn.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+
 mongoose.connect(
   // "mongodb+srv://icakecluster.vzkdm.mongodb.net/icakedb",
-  "mongodb://localhost:27017/icakedb",
+  // "mongodb://localhost:27017/icakedb",
+  "mongodb+srv://icake:icake2021@icakecluster.vzkdm.mongodb.net/icakedb?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
