7df0a0a114284bd78efcb12108e3b0f73a9901f3
gfrancoa <gfrancoa@outlook.com>
2021-07-29 21:26:53 -0500

    actualizacion de puerto con heroku

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var app = require("./app");
 var mongoose = require("./src/conexDB/conn");
 
-var port = 4000;
-
 //el link es localhost:4000/
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("servidor corriendo");
 });
