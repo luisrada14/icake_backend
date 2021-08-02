9c8c268929fb82876d4d3d3eb286ea8f35ffa19d
gfrancoa <gfrancoa@outlook.com>
2021-08-01 21:39:32 -0500

    cambiando numero de puerto

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var mongoose = require("./src/conexDB/conn");
 
 //el link es localhost:4000/
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 56807, () => {
   console.log("servidor corriendo");
 });
