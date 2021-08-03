c2dd56889bb2a430da02fb0133c1988db2629a6c
gfrancoa <gfrancoa@outlook.com>
2021-08-02 23:03:12 -0500

    id añadido a cobertura schema

--- src/models/cobertura.js
+++ src/models/cobertura.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var CoberturaSchema = Schema({
   cobertura_torta: { type: String },
   precio: { type: Number },
+  _id: { type: mongoose.Schema.Types.ObjectId },
 });
 
 const Cobertura = mongoose.model("cobertura", CoberturaSchema);
